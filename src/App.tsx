fa065574af97d206982d5c999266ddabadc2a090
hungdt1509 <hungdangak1509@gmail.com>
2024-04-19 22:58:19 +0700

    First push

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Navbar from "./layouts/Header-Footer/Navbar";
+
+function App() {
+    return (
+        <div className="App">
+            <Navbar></Navbar>
+        </div>
+    );
+}
+
+export default App;
